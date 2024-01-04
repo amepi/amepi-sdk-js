0b133351509433a029c0935da1ab43343f5e6f53
Alexandre Hak <hak.alexandre@gmail.com>
2024-01-04 17:59:57 +0100

    feat: add deno task "test"
    Move CONTRIBUTING.md to .github/

--- src/message-queue/message-queue.ts
+++ src/message-queue/message-queue.ts
@@ -1,8 +1,17 @@
-// import { ReceiveMessageCommand, DeleteMessageCommand, CreateQueueCommand, SQSClient } from "@aws-sdk/client-sqs";
+// import { GetQueueUrlCommand, ReceiveMessageCommand, DeleteMessageCommand, CreateQueueCommand, SQSClient } from "@aws-sdk/client-sqs";
+// @see https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/welcome.html
 class MessageQueue {
   constructor() {}
-
-  listQueue() {}
+  // createQueue(){}
+  // deleteQueue(){}
+  listQueues(){}
+  getQueueUrl(queueName: string): string {
+    // the queue url
+    return '';
+  }
+  // sendMessage(queueName: string, message: string){}
+  // receiveMessages(queueName: string){}
+  // getQueueAttribute() {}
 }
 
 export default MessageQueue;
--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,13 +1,24 @@
+// import {S3} from "@aws-sdk/client-s3";
+// @see https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_s3_code_examples.html
+
 /**
- * Features
- * [ ] upload to storage
- * [ ] get/list storage
+ * AWS S3
+ * 
  */
 export class Storage {
   constructor() {
   }
 
-  getBlob() {
-    
-  }
+  // listBuckets(){}
+  // listBucketObjects(command: ListObjectsV2Command){}
+  getObject() {}
+  // uploadObject(command: PutObjectCommand){
+  //   try {
+  //     const response = await client.send(command);
+  //     console.log(response);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
+  // deleteBlob(){}
 }
